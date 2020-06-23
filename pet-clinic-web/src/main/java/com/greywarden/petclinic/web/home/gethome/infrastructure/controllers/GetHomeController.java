ea681899c43120d5ab0eb8f12bcdefb7c30a4561
GreyWarden <raulpavonfp@gmail.com>
2020-06-23 20:27:18 +0200

    Moved GetHomeController to a better package

--- pet-clinic-web/src/main/java/com/greywarden/petclinic/web/home/gethome/infrastructure/controllers/GetHomeController.java
+++ pet-clinic-web/src/main/java/com/greywarden/petclinic/web/home/gethome/infrastructure/controllers/GetHomeController.java
@@ -1,4 +1,4 @@
-package com.greywarden.petclinic.web.home.gethome.infrastructure;
+package com.greywarden.petclinic.web.home.gethome.infrastructure.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
