cabbb09910012e36ea78c2cdd7ed6dd375b07b4b
GreyWarden <raulpavonfp@gmail.com>
2020-07-01 19:10:46 +0200

    Added Owners List and updated Controller. Closes #17

--- pet-clinic-web/src/main/java/com/greywarden/petclinic/web/owner/showowners/infrastructure/controllers/ShowOwnersController.java
+++ pet-clinic-web/src/main/java/com/greywarden/petclinic/web/owner/showowners/infrastructure/controllers/ShowOwnersController.java
@@ -1,13 +1,24 @@
 package com.greywarden.petclinic.web.owner.showowners.infrastructure.controllers;
 
+import com.greywarden.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public final class ShowOwnersController {
+
+    private OwnerService ownerService;
+
+    public ShowOwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping("/owners")
-    public String showOwners()
+    public String showOwners(Model model)
     {
+        model.addAttribute("owners", this.ownerService.findAll());
+
         return "owners/index";
     }
 }
