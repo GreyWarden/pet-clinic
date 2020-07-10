68780aa0b5ae3d97c92e01897e8bc9bc12d44d89
Raúl Pavón <raulpavonfp@gmail.com>
2020-07-10 19:01:23 +0200

    Now PetTypeMapRepository counts as a service. closes #28

--- pet-clinic-data/src/main/java/com/greydevelopments/petclinic/pet/pettype/infrastructure/repositories/PetTypeMapRepository.java
+++ pet-clinic-data/src/main/java/com/greydevelopments/petclinic/pet/pettype/infrastructure/repositories/PetTypeMapRepository.java
@@ -3,9 +3,11 @@ package com.greydevelopments.petclinic.pet.pettype.infrastructure.repositories;
 import com.greydevelopments.petclinic.pet.pettype.domain.models.PetType;
 import com.greydevelopments.petclinic.pet.pettype.domain.repositories.PetTypeRepository;
 import com.greydevelopments.petclinic.shared.infrastructure.repositories.map.AbstractMapRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public final class PetTypeMapRepository
         extends AbstractMapRepository<PetType, Long>
         implements
