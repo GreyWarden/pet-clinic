3e9d1d88f9788cde08f72848e251747586bb4f85
Raúl Pavón <raulpavonfp@gmail.com>
2020-07-10 18:31:41 +0200

    Created specialty entity and associate it to vet. closes #26

---
+++ pet-clinic-data/src/main/java/com/greywarden/petclinic/models/Specialty.java
@@ -0,0 +1,13 @@
+package com.greywarden.petclinic.models;
+
+public final class Specialty extends BaseEntity {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
