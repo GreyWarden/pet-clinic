2970562857d7d236b2f13943877f705461b4d7d2
Raúl Pavón <raulpavonfp@gmail.com>
2020-07-10 18:41:43 +0200

    Added PetTypeRepository and PetTypeMap repository

---
+++ pet-clinic-data/src/main/java/com/greywarden/petclinic/repositories/map/PetTypeMapRepository.java
@@ -0,0 +1,10 @@
+package com.greywarden.petclinic.repositories.map;
+
+import com.greywarden.petclinic.models.PetType;
+
+public final class PetTypeRepository
+        extends AbstractMapRepository <PetType, Long>
+        implements
+        PetTypeRepository
+{
+}
