56842f8d534ba64ade273c156d1090ca1184786e
Raúl Pavón <raulpavonfp@gmail.com>
2020-07-10 20:15:45 +0200

    If specialty is not saved, Vet save method creates it

--- pet-clinic-data/src/main/java/com/greydevelopments/petclinic/person/vet/vet/infrastructure/repositories/map/VetMapRepository.java
+++ pet-clinic-data/src/main/java/com/greydevelopments/petclinic/person/vet/vet/infrastructure/repositories/map/VetMapRepository.java
@@ -1,5 +1,7 @@
 package com.greydevelopments.petclinic.person.vet.vet.infrastructure.repositories.map;
 
+import com.greydevelopments.petclinic.person.vet.specialty.domain.models.Specialty;
+import com.greydevelopments.petclinic.person.vet.specialty.domain.repositories.SpecialtyRepository;
 import com.greydevelopments.petclinic.person.vet.vet.domain.models.Vet;
 import com.greydevelopments.petclinic.person.vet.vet.domain.repositories.VetRepository;
 import com.greydevelopments.petclinic.shared.infrastructure.repositories.map.AbstractMapRepository;
@@ -12,6 +14,12 @@ public final class VetMapRepository
         extends AbstractMapRepository<Vet, Long>
         implements VetRepository
 {
+    SpecialtyRepository specialtyRepository;
+
+    public VetMapRepository(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -29,6 +37,15 @@ public final class VetMapRepository
 
     @Override
     public Vet save(Vet object) {
+        Set<Specialty> specialties = object.getSpecialties();
+        if(specialties.size() > 0) {
+            specialties.forEach(specialty -> {
+                if(specialty.getId() != null) {
+                    return;
+                }
+                specialty.setId(specialtyRepository.save(specialty).getId());
+            });
+        }
         return super.save(object);
     }
 
